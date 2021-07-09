02816f2d422b07407dc405b0ffa612cc72b18bb7
priyadarshanvj <63783259+priyadarshanvj@users.noreply.github.com>
2021-07-09 20:37:16 +0530

    Update Stat_system_call.c

--- Stat_system_call.c
+++ Stat_system_call.c
@@ -9,10 +9,10 @@ if(stat(“notes.txt”,&statv)==-1)
 perror(“stat”);
 else
 {
-printf(“\n The file user id is: %d”,statv.st_uid); 
-printf(“\n The file group id is: %d”,statv.st_gid);
-printf(„\n The file size is :%d”,statv.st_size);
-printf(“\n The file inode number is :%d”,statv.st_ino);
+printf("\n The file user id is: %d",statv.st_uid); 
+printf("\n The file group id is: %d",statv.st_gid);
+printf("\n The file size is :%d",statv.st_size);
+printf("\n The file inode number is :%d",statv.st_ino);
 }
 return 0;
 }
